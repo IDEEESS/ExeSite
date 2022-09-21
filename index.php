acea3a3790a503301afe23ac63d9ebed569e2bfc
IlannMusici <ilanndesnoes@gmail.com>
2022-09-21 19:39:57 +0200

    Ajout du dessous du bg image

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>Exellia - Pvp Faction moddé</title>
 </head>
 <div class="navbar">
@@ -15,6 +15,19 @@
 
 	</header><br>
 <body>
-	<h2 class="jouer">Jouer</h2>
+	<h2 class="jouer" align="center">Jouer</h2>
+	<a href="#" class="redirectDownload" align="center">Télécharger le launcher</a><br><br><br><br>
+	<hr><br><br>
+	<div align="left" class="article1">
+
+		<h3>Article sur la congolexicomatisation des lois du marché</h3><br>
+		<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+
+	</div>
 </body>
 </html>
