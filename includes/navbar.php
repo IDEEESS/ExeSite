0f707b45f1dba5775ec369eb325808f006d06239
IlannMusici <ilanndesnoes@gmail.com>
2022-09-21 13:33:47 +0200

    Ajout de la nav bar non fonctionnelle et du background-image

---
+++ includes/navbar.php
@@ -0,0 +1,32 @@
+
+<nav>
+
+	<ul>
+		<h1 class="title">Exellia</h1>
+		<li>
+
+			<a href="#">ACCUEIL</a>
+
+		</li>
+
+		<li>
+
+			<a href="#">CONTACT</a>
+
+		</li>
+
+		<li>
+
+			<a href="#">TELECHARGER</a>
+
+		</li>
+
+		<li>
+
+			<a href="#">SE CONNECTER</a>
+
+		</li>
+
+	</ul>
+
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>Exellia - Pvp Faction moddé</title>
+</head>
+<body>
+	<div class="navbar">
+		<?php include 'includes/navbar.php';?>
+	</div>
+	<header class="header">
+
+		<h1 class="title-header">Bienvenue sur Exellia</h1>
+
+	</header>
+</body>
+</html>
